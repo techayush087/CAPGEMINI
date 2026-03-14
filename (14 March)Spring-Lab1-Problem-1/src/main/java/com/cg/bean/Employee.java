7c220c52db118376fe582d834bd5c6d97daaa055
Ayush <chaudharyayush028@gmail.com>
2026-03-14 13:53:22 +0530

    14 MARCH SPRING LAB-1 ASSIGNMENT-1.1

---
+++ (14 March)Spring-Lab1-Problem-1/src/main/java/com/cg/bean/Employee.java
@@ -0,0 +1,70 @@
+package com.cg.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component
+public class Employee {
+	@Value("${empid}")
+     private int empid;
+	@Value("${name}")
+     private String name;
+	@Value("${salary}")
+     private double salary;
+	@Value("${BU}")
+     private String BU;
+	@Value("${age}")
+     private int age;
+     
+   
+     
+	 public Employee() {
+		 
+		
+	}
+	 public int getEmpid() {
+		 return empid;
+	 }
+	 public void setEmpid(int empid) {
+		 this.empid = empid;
+	 }
+	 public String getName() {
+		 return name;
+	 }
+	 public void setName(String name) {
+		 this.name = name;
+	 }
+	 public double getSalary() {
+		 return salary;
+	 }
+	 public void setSalary(double salary) {
+		 this.salary = salary;
+	 }
+	 public String getBU() {
+		 return BU;
+	 }
+	 public void setBU(String bU) {
+		 BU = bU;
+	 }
+	 public int getAge() {
+		 return age;
+	 }
+	 public void setAge(int age) {
+		 this.age = age;
+	 }
+	 
+	 
+	 public void printEmployee() {
+		  System.out.println("Employee Details");
+		   System.out.println("-------------------------");
+		   System.out.println("Employee Id : "+getEmpid());
+		   System.out.println("Employee Name : "+getName());
+		   System.out.println("Employee Salary : "+getSalary());
+		   System.out.println("Employee BU: "+getBU());
+		   System.out.println("Employee age: "+getAge());
+		
+	 }
+	 
+	
+     
+     
+}
